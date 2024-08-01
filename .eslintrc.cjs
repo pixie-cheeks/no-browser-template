119283792d0493f8955136a7927ef67e6692bb78
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-08-02 01:13:58 +0530

    Update template and remove redundant parts

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: false,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  extends: ['airbnb-base', 'prettier'],
+  ignorePatterns: ['/*', '!/src'],
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+
+    'lines-between-class-members': [
+      'error',
+      {
+        enforce: [
+          { blankLine: 'always', prev: '*', next: 'method' },
+          { blankLine: 'always', prev: 'method', next: '*' },
+          { blankLine: 'never', prev: 'field', next: 'field' },
+        ],
+      },
+    ],
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+    'import/extensions': ['error', 'ignorePackages'],
+  },
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true,
+      },
+      extends: ['plugin:jest/recommended'],
+      plugins: ['jest'],
+    },
+  ],
+};
